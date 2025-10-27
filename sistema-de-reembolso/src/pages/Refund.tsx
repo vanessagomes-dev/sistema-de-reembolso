529515781d1845c73e501a34df820ece3d454a1d
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-27 19:05:46 -0300

    feat(routes): criar rotas para colaboradores e página de solicitações de reembolso
    Implementadas as rotas específicas para os funcionários da aplicação no arquivo employ-routes.tsx, utilizando o React Router para gerenciar a navegação.
    Criada a página Refound, onde os colaboradores poderão realizar suas solicitações de reembolso, estabelecendo a base para o fluxo funcional do perfil de usuário comum.

---
+++ sistema-de-reembolso/src/pages/Refund.tsx
@@ -0,0 +1,8 @@
+export function Refund() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Página de Reembolso</h1>
+      <p className="text-lg text-gray-700">Aqui você pode solicitar um reembolso.</p>
+    </div>
+  );
+}
--- sistema-de-reembolso/src/routes/index.tsx
+++ sistema-de-reembolso/src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router";
 import { AuthRoutes } from "./AuthRoutes";
+import { EmployeeRoutes } from "./EmployeeRoutes";
 
 export function Routes(){
     return(
         <BrowserRouter>
-            <AuthRoutes/>
+            <EmployeeRoutes />
         </BrowserRouter>
     )
 }

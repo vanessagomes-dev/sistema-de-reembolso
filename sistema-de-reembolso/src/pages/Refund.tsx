1372d8469f38b977f3a757d17d63e1750928901c
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-27 19:23:00 -0300

    feat(layout): criar layout compartilhado para rotas de employee e manager
    Desenvolvido o componente AppLayout.tsx na pasta components, responsável por estruturar o layout base da aplicação.
    Incluído o Outlet do React Router para renderização dinâmica das páginas internas e adicionado suporte responsivo com Tailwind CSS.
    Aplicado o layout nas rotas, permitindo o compartilhamento de elementos comuns, como o cabeçalho, entre diferentes perfis de usuário.

--- sistema-de-reembolso/src/pages/Refund.tsx
+++ sistema-de-reembolso/src/pages/Refund.tsx
@@ -1,8 +1,3 @@
 export function Refund() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Página de Reembolso</h1>
-      <p className="text-lg text-gray-700">Aqui você pode solicitar um reembolso.</p>
-    </div>
-  );
+  return <h1>Página de Reembolso</h1>;
 }
--- sistema-de-reembolso/src/routes/EmployeeRoutes.tsx
+++ sistema-de-reembolso/src/routes/EmployeeRoutes.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route} from "react-router"
-
+ 
 import { Refund } from "../pages/Refund"
 import { NotFound } from "../pages/NotFound"
+import { AppLayout } from "../components/AppLayout"
 
 export function EmployeeRoutes() {
     return (
         <Routes>
+            <Route path="/" element={<AppLayout />} >
             <Route path="/" element={<Refund />} />
+            </Route>
+
             <Route path="*" element={<NotFound />} />
         </Routes>
     )

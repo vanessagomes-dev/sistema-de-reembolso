708b3e493499e6d15a46971d6db94944694d955c
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-27 16:49:17 -0300

    feat(form): criar componente de input personalizado e aplicar estilização dinâmica
    Removido o título anterior e substituído por um componente de input totalmente estilizado, ocupando 100% da largura disponível.
    Criado um componente de input tipado, com suporte a propriedades personalizadas como legenda opcional.
    Adicionados estilos dinâmicos com Tailwind CSS, incluindo destaque visual ao focar no campo.
    Duplicado o componente para criação do campo de senha, mantendo as mesmas configurações e consistência visual.

--- sistema-de-reembolso/src/pages/SignIn.tsx
+++ sistema-de-reembolso/src/pages/SignIn.tsx
@@ -1,3 +1,22 @@
-export function SignIn(){
-  return <h1>SignIn</h1>
+import { Input } from "../components/Input";
+
+export function SignIn() {
+  return (
+    <form className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+      />
+
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="digite sua senha"
+      />
+
+    </form>
+  );
 }

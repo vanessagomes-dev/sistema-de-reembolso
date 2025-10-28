b4e5b95df9411cfade2adfa1ac12d2755ad7bad4
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-28 17:26:59 -0300

    feat(form): criar componente de select baseado no input existente
    Duplicado o componente de input e renomeado para Select, ajustando as propriedades para suportar opções passadas como filhos.
    Implementada uma opção padrão "Selecione" e personalizado o ícone da setinha do select via CSS.
    O componente mantém consistência visual com o restante do formulário e amplia a flexibilidade da interface.

--- sistema-de-reembolso/src/pages/Refund.tsx
+++ sistema-de-reembolso/src/pages/Refund.tsx
@@ -1,3 +1,20 @@
+import { Input } from "../components/Input";
+import { Select } from "../components/Select";
+
 export function Refund() {
-  return <h1>Página de Reembolso</h1>
+  return (
+    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+      <header>
+        <h1 className="text-xl font-bold text-gray-100">
+          Solicitação de reembolso
+        </h1>
+        <p className="text-sm text-gray-200 mt-2 mb-4">
+          Dados da despesa para solicitar reembolso.
+        </p>
+      </header>
+
+      <Input required legend="Nome da solicitação" />
+      <Select required legend="Categoria" />
+    </form>
+  );
 }

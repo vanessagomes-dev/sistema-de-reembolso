34803a6852b13f42ff424ef4ccc8c9ff867e3fd6
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-28 19:51:02 -0300

    feat(routes): adiciona página de confirmação e navegação pós-envio do formulário
    - Criado arquivo confirm.tsx com componente de título
    - Ajustadas as rotas para redirecionar após envio do formulário
    - Implementada verificação com useNavigate e useLocation para impedir acesso direto à página

--- sistema-de-reembolso/src/pages/Refund.tsx
+++ sistema-de-reembolso/src/pages/Refund.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
 import { Upload } from "../components/Upload";
@@ -12,8 +13,12 @@ export function Refund() {
   const [isLoading, setIsLoading] = useState(false);
   const [filename, setFilename] = useState<File | null>();
 
+  const navigate = useNavigate()
+
   function onSubmit(e: React.FormEvent) {
     e.preventDefault();
+
+    navigate("/confirm", { state: { fromSubmit: true } })
   }
 
   return (

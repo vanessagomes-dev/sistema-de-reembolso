bc176b71b0747cb1c218df863a43bb60f3586556
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-29 11:49:08 -0300

    feat(routes): adiciona página Dashboard e estrutura de rotas do gerenciador
    - Criada nova página `dashboard.tsx` com título inicial
    - Configurado arquivo `manager.routes.tsx` importando Dashboard e demais componentes
    - Definidas rotas principais e rota para páginas não encontradas
    - Atualizado `index.tsx` para incluir a nova estrutura de rotas

--- sistema-de-reembolso/src/routes/index.tsx
+++ sistema-de-reembolso/src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from "react-router";
 import { AuthRoutes } from "./AuthRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 
 export function Routes(){
     return(
         <BrowserRouter>
-            <EmployeeRoutes />
+            <ManagerRoutes />
         </BrowserRouter>
     )
 }
